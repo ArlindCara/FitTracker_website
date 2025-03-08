0a87f38df74f87aabcdfeb7f8cfcd7d07ed0bee3
Arlind Cara <49277331+ArlindCara@users.noreply.github.com>
2025-03-08 10:50:18 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,106 @@
+$(document).ready(function(){
+    /*-----NavBar Shrink ------ */
+    $(window).on("scroll", function(){
+        if($(this).scrollTop() > 90){
+            $(".navbar").addClass("navbar-shrink");
+        }
+        else{
+            $(".navbar").removeClass("navbar-shrink");
+        }
+    });
+    /*--- Video Popup ----*/
+    const videoSrc = $("#player-1").attr("src");
+    $(".video-play-btn, .video-popup").on("click", function(){
+        if($(".video-popup").hasClass("open")){
+            $(".video-popup").removeClass("open");
+            $("#player-1").attr("src", "");
+        }
+        else{
+            $(".video-popup").addClass("open");
+            if($("#player-1").attr("src")==''){
+                $("#player-1").attr("src",videoSrc);
+            }
+        }
+    }); 
+
+/*---------- features Carousel----------- */
+$('.features-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    /*sposta il carosello in automatico*/
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+/*---------- screenshots Carousel----------- */
+$('.screenshots-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    /*sposta il carosello in automatico*/
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+/*---------- testimonials Carousel----------- */
+$('.testimonials-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    /*sposta il carosello in automatico*/
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+/*---------- team Carousel----------- */
+$('.team-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    /*sposta il carosello in automatico*/
+    autoplay: true,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+
+});
